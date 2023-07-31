8853c47ceb8b124b085d6fc5904874e70742f1e0
mkukiro <jordanparisi07@gmail.com>
2023-07-31 20:39:37 +0200

    ✨ Show username and hostname in the prompt

--- squish.rkt
+++ squish.rkt
@@ -3,10 +3,10 @@
 (require racket/system)
 
 (define (squish)
-  (printf "┌ \033[36m~a\033[0m\n" (current-directory))
+  (printf "┌ \033[35m~a\033[0m@\033[35m~a\033[0m \033[32m~a\033[0m ☯\n" (getenv "USER") (getenv "HOSTNAME") (current-directory))
   (display "└ \033[33mλ \033[0m")
   (flush-output)
-  
+
   (let ([x (read-line)])
     (cond
       ((equal? x "exit") (exit))
