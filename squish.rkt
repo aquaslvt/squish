cfced8fa013492cac868eac8daefbec64355d019
mkukiro <jordanparisi07@gmail.com>
2023-08-03 09:37:29 +0200

    ✨ `/home/user/` now displays as `~/`

--- squish.rkt
+++ squish.rkt
@@ -3,7 +3,11 @@
 (require racket/system)
 
 (define (squish)
-  (printf "┌ \033[35m~a\033[0m@\033[35m~a\033[0m \033[32m~a\033[0m ☯\n" (getenv "USER") (getenv "HOSTNAME") (current-directory))
+  (printf "┌ \033[35m~a\033[0m@\033[35m~a\033[0m \033[32m~a\033[0m ☯\n" 
+    (getenv "USER")
+    (getenv "HOSTNAME") 
+    (regexp-replace #rx"/home/[^/]+" (path->string (current-directory)) "~"))
+  
   (display "└ \033[33mλ \033[0m")
   (flush-output)
 
