0c2a36fe03b77e44e8c8d009c9aa4b262637917c
mkukiro <jordanparisi07@gmail.com>
2023-08-04 15:50:54 +0200

    ✨ Added `cd`

--- squish.rkt
+++ squish.rkt
@@ -3,7 +3,10 @@
 (provide squish-run)
 
 (define (squish-run command)
-    (cond
-      ((equal? command "exit") (exit))
-      (else (system command))
-    ))
+  (cond
+    ((string-contains? command "cd ")
+     (if (directory-exists? (substring command 3))
+         (current-directory (substring command 3))
+         (displayln "Directory not found")))
+    ((equal? command "exit") (exit))
+    (else (system command))))
