52cd1d19c7f97c2167be9686da39e9ef01ce61db
mkukiro <jordanparisi07@gmail.com>
2023-08-05 15:17:46 +0200

    💩 Added `&`

--- squish.rkt
+++ squish.rkt
@@ -8,8 +8,13 @@
 (define (squish-run command)
   (cond
     ((string-contains? command "cd ")
-     (if (directory-exists? (substring command 3))
-         (current-directory (substring command 3))
-         (displayln "Directory not found")))
+      (if (directory-exists? (substring command 3))
+          (current-directory (substring command 3))
+          (displayln "Directory not found")))
+    
+    ((string-contains? command " & ")
+      (system (substring command 0 2))
+      (system (substring command 5)))
+
     ((equal? command "exit") (exit))
     (else (system command))))
