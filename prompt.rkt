3c007c5dc53004d8feb0f8b55ef382a3cfd301a2
mkukiro <jordanparisi07@gmail.com>
2023-08-13 19:07:08 +0200

    🚧 Made `config-ref` work

--- prompt.rkt
+++ prompt.rkt
@@ -4,15 +4,16 @@
          toml
          racket/file)
 
-;; (define (config-ref section value)
-;;     (hash-ref (hash-ref (parse-toml (file->string "config.toml")) section) value))
+(define (config-ref section value)
+    (hash-ref (hash-ref (parse-toml (file->string "config.toml")) (string->symbol section)) (string->symbol value)))
+    ;; Is gonna get last-threaded soon
 
 (define (prompt)
   (printf "┌ \033[35m~a\033[0m@\033[35m~a\033[0m \033[32m~a\033[0m ~a\n" 
     (getenv "USER")
     (getenv "HOSTNAME") 
     (regexp-replace #rx"/home/[^/]+" (path->string (current-directory)) "~")
-    (hash-ref (hash-ref (parse-toml (file->string "config.toml")) 'prompt) 'extra_symbols))
+    (config-ref "prompt" "extra_symbols"))
   
   (display "└ \033[33mλ \033[0m")
   (flush-output)
