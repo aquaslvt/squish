00e310e7582e3d8cebc94e494c324943bcdb3d91
mkukiro <jordanparisi07@gmail.com>
2023-08-13 09:32:35 +0200

    🚧 Added `extra_symbols` as configuration

--- prompt.rkt
+++ prompt.rkt
@@ -1,12 +1,18 @@
 #lang racket/base
 
-(require "squish.rkt")
+(require "squish.rkt"
+         toml
+         racket/file)
+
+;; (define (config-ref section value)
+;;     (hash-ref (hash-ref (parse-toml (file->string "config.toml")) section) value))
 
 (define (prompt)
-  (printf "┌ \033[35m~a\033[0m@\033[35m~a\033[0m \033[32m~a\033[0m ☯\n" 
+  (printf "┌ \033[35m~a\033[0m@\033[35m~a\033[0m \033[32m~a\033[0m ~a\n" 
     (getenv "USER")
     (getenv "HOSTNAME") 
-    (regexp-replace #rx"/home/[^/]+" (path->string (current-directory)) "~"))
+    (regexp-replace #rx"/home/[^/]+" (path->string (current-directory)) "~")
+    (hash-ref (hash-ref (parse-toml (file->string "config.toml")) 'prompt) 'extra_symbols))
   
   (display "└ \033[33mλ \033[0m")
   (flush-output)
